1acceec09b7ced34d11f3a28d372f30996043ca5
Quan01781 <nguyenhoangzeroquan@gmail.com>
2022-10-15 17:41:38 +0700

    add index view and new controller

--- CustomerSite/ecommerce/Controllers/HomeController.cs
+++ CustomerSite/ecommerce/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using ecommerce.Models;
+using API.Services;
 
 namespace ecommerce.Controllers
 {
---
+++ CustomerSite/ecommerce/Controllers/ProductsController.cs
@@ -0,0 +1,60 @@
+﻿using API.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CustomerSite.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public ProjectServices _projectServices;
+        //private ShopDbContext _context;
+
+        public ProductsController(ProjectServices projectServices)
+        {
+            _projectServices = projectServices;
+        }
+
+        [HttpGet("get-all-products")]
+        public IActionResult GetAllProducts()
+        {
+            var allProducts = _projectServices.GetAllProducts();
+
+            return Ok(allProducts);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetProductById(int id)
+        {
+            var Product = _projectServices.GetProductById(id);
+
+            return Ok(Product);
+        }
+
+        //[HttpPost("add-product")]
+        //public IActionResult AddProduct(Products product)
+        //{
+        //    _projectServices.AddProduct(product);
+        //    return Ok();
+        //}
+
+
+
+        //[HttpPost]
+        //public async Task<ActionResult<Products>> PostProducts(Products productitem)
+        //{
+        //    _context.Products.Add(productitem);
+        //    await _context.SaveChangesAsync();
+
+        //    //return CreatedAtAction("GetTodoItem", new { id = todoItem.Id }, todoItem);
+        //    return CreatedAtAction(nameof(GetTodoItem), new { id = productitem.ID }, productitem);
+        //}
+
+    }
+
+}
