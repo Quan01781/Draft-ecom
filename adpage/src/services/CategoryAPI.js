78ec9e6a7c34174360c5b020c29bdacf43e11b6e
Quan01781 <nguyenhoangzeroquan@gmail.com>
2022-11-07 22:08:25 +0700

    admin page

---
+++ adpage/src/services/CategoryAPI.js
@@ -0,0 +1,57 @@
+import axios from "axios";
+const url = "https://localhost:7243/api/category/";
+// eslint-disable-next-line import/no-anonymous-default-export
+export async function GetCategory() {
+  try {
+    let result = await axios.get(url+'get-all-categories');
+    console.log(result);
+    return result;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function AddCategory(categoryName){
+  try {
+    let category={
+      name: categoryName,
+      created_by: null,
+      created_at: null,
+      updated_at: null
+    };
+    let result = await axios.post(url+'add-category', category);
+    console.log(result);
+    return result;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function UpdateCategory(categoryName, categoryID){
+  try {
+    let category={
+      name: categoryName,
+      created_by: null,
+      created_at: null,
+      updated_at: null
+    };
+    let result = await axios.put(url+`update-category/${categoryID}`, category);
+    console.log(result);
+    return result;
+  } catch (error) {
+    console.log(error);
+  }
+
+}
+
+
+export async function DeleteCategory(categoryID){
+  try {
+    let result = await axios.delete(url+`delete-category/${categoryID}`);
+    console.log(result);
+    return result;
+  } catch (error) {
+    console.log(error);
+  }
+
+}
