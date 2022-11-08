b0a5c9c9b22dc046497ac29221077ae3c070274e
Quan01781 <nguyenhoangzeroquan@gmail.com>
2022-11-08 15:57:33 +0700

    modify code

--- adpage/src/pages/viewDetailProduct.js
+++ adpage/src/pages/viewDetailProduct.js
@@ -1,5 +1,4 @@
 import React, {useEffect, useState} from "react";
-import Card from 'react-bootstrap/Card';
 import {GetProductByID} from '../services/ProductAPI'
 
 export default function DetailProduct(){
@@ -16,17 +15,26 @@ const result = await GetProductByID(productID);
 console.log(result.data)
 setProduct(result.data);}
 
+const url = "https://localhost:7243/wwwroot/upload/image/product/" + product.image;
 return (
+
     <>
-    <Card className="detail-category" style={{width: '60%'}}>
-    <Card.Header>{product.name}</Card.Header>
-      <Card.Body>
-        <Card.Text>Price: {product.price}</Card.Text>
-        <Card.Text>Description: {product.description}</Card.Text>
-        <Card.Text>Created Date: {product.created_at}</Card.Text>
-        <Card.Text>Updated Date: {product.updated_at}</Card.Text>
-      </Card.Body>
-    </Card>
+    <div className="detail-category card mb-3" style={{width: '60%'}}>
+  <div className="row g-0">
+    <div className="col-md-4">
+      <img src={url} className="img-fluid rounded-start" alt="..."/>
+    </div>
+    <div className="col-md-8">
+      <div className="card-body">
+        <h5 className="card-title">{product.name}</h5>
+        <p className="card-text">Price: {product.price}</p>
+        <p className="card-text"><small class="text-muted"><b>Description:</b> {product.description}</small></p>
+        <p className="card-text"><small class="text-muted"><b>Created Date:</b> {product.created_at}</small></p>
+        <p className="card-text"><small class="text-muted"><b>Updated Date:</b> {product.updated_at}</small></p>
+      </div>
+    </div>
+  </div>
+</div>
     </>
 );
 }
