ec230d2d69267d90994f4020f68919e227447eca
Quan01781 <nguyenhoangzeroquan@gmail.com>
2022-11-15 22:23:42 +0700

    fix code

--- API/ShareViewModel/DTO/AdminProductDTO.cs
+++ API/ShareViewModel/DTO/AdminProductDTO.cs
@@ -10,6 +10,8 @@ namespace ShareViewModel.DTO
         public int? CategoryID { get; set; }
         public string? Image { get; set; }
         public string? Description { get; set; }
+        public string Color { get; set; }
+        public string Size { get; set; }
         public string? Created_by { get; set; }
     }
 }
